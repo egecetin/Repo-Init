a76c8ba5c291885eae181b983b94dad3cad03fe7
egecetin <egecetin@hotmail.com.tr>
2022-04-16 17:54:06 +0300

    doxy

---
+++ WorkDirectory/cmake/Doxy.cmake
@@ -0,0 +1,18 @@
+find_package(Doxygen)
+if (DOXYGEN_FOUND)
+    # set input and output files
+    set(DOXYGEN_IN ${PROJECT_SOURCE_DIR}/doc/Doxyfile.in)
+    set(DOXYGEN_OUT ${PROJECT_SOURCE_DIR}/Doxyfile)
+
+    # request to configure the file
+    configure_file(${DOXYGEN_IN} ${DOXYGEN_OUT} @ONLY)
+
+    # note the option ALL which allows to build the docs together with the application
+    add_custom_target( docs ALL
+        COMMAND ${DOXYGEN_EXECUTABLE} ${DOXYGEN_OUT}
+        WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+        COMMENT "Generating API documentation with Doxygen"
+        VERBATIM )
+else (DOXYGEN_FOUND)
+  message("${BoldYellow}Doxygen need to be installed to generate the doxygen documentation!${ColourReset}")
+endif (DOXYGEN_FOUND)
