29b0b049ad133ddda9f47c905dfcf8ad0e37942f
egecetin <egecetin@hotmail.com.tr>
2022-04-16 18:21:28 +0300

    update

--- WorkDirectory/scripts/init.sh
+++ WorkDirectory/scripts/init.sh
@@ -3,4 +3,3 @@
 # Create empty directories
 mkdir -p build cmake doc dist example include scripts src tests thirdparty
 touch build/.keep cmake/.keep doc/.keep dist/.keep example/.keep include/.keep scripts/.keep src/.keep tests/.keep thirdparty/.keep
-
--- package.sh
+++ package.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 sh makeself/makeself.sh --notemp --sha256 WorkDirectory/ "RepoInit.run" "Repo Initializer" sh scripts/init.sh
+mkdir -p Dist
+mv RepoInit.run Dist/
