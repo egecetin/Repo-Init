11b9ef324a5b016db4ee2e94422afabcdbc15cbb
egecetin <egecetin@hotmail.com.tr>
2022-05-29 23:47:08 +0300

    fix typos package and precommit

--- tests/gtest_main.cpp
+++ tests/gtest_main.cpp
@@ -8,25 +8,26 @@ int main(int argc, char **argv)
 	::testing::InitGoogleTest(&argc, argv);
 	int retval = EXIT_FAILURE;
 
-    // Log triggers memleak warnings
-    spdlog::set_level(spdlog::level::off);
+	// Log triggers memleak warnings
+	spdlog::set_level(spdlog::level::off);
 
 	MemPlumber::start();
 	retval = RUN_ALL_TESTS();
 	MemPlumber::stop();
 
-    // Check memory leak
+	// Check memory leak
 	size_t memLeakCtr;
 	uint64_t memLeakSz;
 	MemPlumber::memLeakCheck(memLeakCtr, memLeakSz, true);
 
-	if(memLeakCtr == 1 && memLeakSz == 8)
-	    printf("No memory leak detected! (Only os_stack_trace_getter_ from"
-	    " googletest-src/googletest/src/gtest.cc:6171)\n");
-	else if(memLeakCtr || memLeakSz)
+	if (memLeakCtr == 1 && memLeakSz == 8)
+		printf("No memory leak detected! (Only os_stack_trace_getter_ from"
+			   " googletest-src/googletest/src/gtest.cc:6171)\n");
+	else if (memLeakCtr || memLeakSz)
 	{
-	    printf("Number of leaked objects: %d\nTotal amount of memory leaked: %d[bytes]\n", (int)memLeakCtr,
-	    (int)memLeakSz); retval = EXIT_FAILURE;
+		printf("Number of leaked objects: %d\nTotal amount of memory leaked: %d[bytes]\n", (int)memLeakCtr,
+			   (int)memLeakSz);
+		retval = EXIT_FAILURE;
 	}
 
 	return retval;
