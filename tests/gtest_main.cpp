cda9a6b89155f1c9e19d5efde7b1524da1adc869
egecetin <egecetin@hotmail.com.tr>
2022-05-29 18:53:40 +0300

    fix and enable memplumber

--- tests/gtest_main.cpp
+++ tests/gtest_main.cpp
@@ -1,30 +1,33 @@
-// #include <memplumber.h>
-#include <gtest/gtest.h>
+#include <memplumber.h>
 #include <spdlog/spdlog.h>
+
+#include <gtest/gtest.h>
 
 int main(int argc, char **argv)
 {
 	::testing::InitGoogleTest(&argc, argv);
 	int retval = EXIT_FAILURE;
 
+    // Log triggers memleak warnings
     spdlog::set_level(spdlog::level::off);
 
-	// MemPlumber::start();
+	MemPlumber::start();
 	retval = RUN_ALL_TESTS();
-	// MemPlumber::stop();
+	MemPlumber::stop();
 
-	// size_t memLeakCtr;
-	// uint64_t memLeakSz;
-	// MemPlumber::memLeakCheck(memLeakCtr, memLeakSz, true);
+    // Check memory leak
+	size_t memLeakCtr;
+	uint64_t memLeakSz;
+	MemPlumber::memLeakCheck(memLeakCtr, memLeakSz, true);
 
-	// if(memLeakCtr == 1 && memLeakSz == 8)
-	//     printf("No memory leak detected! (Only os_stack_trace_getter_ from
-	//     googletest-src/googletest/src/gtest.cc:6171)\n");
-	// else if(memLeakCtr || memLeakSz)
-	// {
-	//     printf("Number of leaked objects: %d\nTotal amount of memory leaked: %d[bytes]\n", (int)memLeakCtr,
-	//     (int)memLeakSz); retval = EXIT_FAILURE;
-	// }
+	if(memLeakCtr == 1 && memLeakSz == 8)
+	    printf("No memory leak detected! (Only os_stack_trace_getter_ from"
+	    " googletest-src/googletest/src/gtest.cc:6171)\n");
+	else if(memLeakCtr || memLeakSz)
+	{
+	    printf("Number of leaked objects: %d\nTotal amount of memory leaked: %d[bytes]\n", (int)memLeakCtr,
+	    (int)memLeakSz); retval = EXIT_FAILURE;
+	}
 
 	return retval;
 }
