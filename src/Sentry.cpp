b9e2da2ebf76d1ed867a4e8f5a35f6540ce954f9
Ege Çetin <egecetin@hotmail.com.tr>
2022-07-27 15:37:36 +0000

    Set sentry context release version

--- src/Sentry.cpp
+++ src/Sentry.cpp
@@ -32,6 +32,7 @@ namespace spdlog
 
 			// Set options
 			sentry_options_t *sentryOptions = sentry_options_new();
+			sentry_options_set_release(sentryOptions, PROJECT_FULL_REVISION);
 			sentry_options_set_dsn(sentryOptions, sentryAddress.c_str());
 			sentry_options_set_symbolize_stacktraces(sentryOptions, true);
 
