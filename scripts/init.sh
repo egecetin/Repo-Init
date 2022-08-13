4b459d14617c887ceb0b91b2429ee29360bb2e53
Ege Çetin <egecetin@hotmail.com.tr>
2022-08-13 14:24:09 +0000

    remove debug directories

--- scripts/init.sh
+++ scripts/init.sh
@@ -2,5 +2,5 @@
 
 # Create empty directories
 mkdir -p build cmake doc dist example include scripts src tests thirdparty
-mkdir -p dist/packages dist/makeself dist/temp dist/debug
+mkdir -p dist/packages dist/makeself dist/temp
 touch build/.gitkeep cmake/.gitkeep doc/.gitkeep dist/.gitkeep example/.gitkeep include/.gitkeep scripts/.gitkeep src/.gitkeep tests/.gitkeep thirdparty/.gitkeep
