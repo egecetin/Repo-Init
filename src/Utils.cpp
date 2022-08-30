5b0f31db299688da2eb55d110fc3afdaa63d1236
Ege Çetin <egecetin@hotmail.com.tr>
2022-08-30 15:20:15 +0000

    fix log level issue

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -56,6 +56,19 @@ bool init_logger(int argc, char **argv)
 	spdlog::set_pattern("[%Y-%m-%d %H:%M:%S.%e] [" + std::string(PROJECT_NAME) + "] [%^%l%$] : %v");
 	print_version();
 
+	// Prepare spdlog loggers
+	auto dup_filter = std::make_shared<spdlog::sinks::dup_filter_sink_mt>(std::chrono::seconds(5));
+	dup_filter->add_sink(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
+	dup_filter->add_sink(std::make_shared<spdlog::sinks::rotating_file_sink_mt>("error.log", 1048576 * 5, 3, false));
+	dup_filter->add_sink(std::make_shared<spdlog::sinks::syslog_sink_mt>(PROJECT_NAME, LOG_USER, 0, false));
+	dup_filter->add_sink(
+		std::make_shared<spdlog::sinks::sentry_api_sink_mt>(readSingleConfig(CONFIG_FILE_PATH, "SENTRY_ADDRESS")));
+
+	// Register main logger
+	auto combined_logger = std::make_shared<spdlog::logger>(PROJECT_NAME, dup_filter);
+	spdlog::set_default_logger(combined_logger);
+	spdlog::warn("{} started", PROJECT_NAME);
+	
 #ifdef NDEBUG
 	spdlog::set_level(spdlog::level::warn);
 #else
@@ -70,19 +83,6 @@ bool init_logger(int argc, char **argv)
 		spdlog::set_level(spdlog::level::debug);
 	if (input.cmdOptionExists("-vvv"))
 		spdlog::set_level(spdlog::level::trace);
-
-	// Prepare spdlog loggers
-	auto dup_filter = std::make_shared<spdlog::sinks::dup_filter_sink_mt>(std::chrono::seconds(5));
-	dup_filter->add_sink(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
-	dup_filter->add_sink(std::make_shared<spdlog::sinks::rotating_file_sink_mt>("error.log", 1048576 * 5, 3, false));
-	dup_filter->add_sink(std::make_shared<spdlog::sinks::syslog_sink_mt>(PROJECT_NAME, LOG_USER, 0, false));
-	dup_filter->add_sink(
-		std::make_shared<spdlog::sinks::sentry_api_sink_mt>(readSingleConfig(CONFIG_FILE_PATH, "SENTRY_ADDRESS")));
-
-	// Register main logger
-	auto combined_logger = std::make_shared<spdlog::logger>(PROJECT_NAME, dup_filter);
-	spdlog::set_default_logger(combined_logger);
-	spdlog::warn("{} started", PROJECT_NAME);
 
 	return true;
 }
