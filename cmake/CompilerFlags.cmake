0245bd697d363365ebd034ae57b58e90d4433cfb
Ege Çetin <egecetin@hotmail.com.tr>
2022-09-18 12:05:00 +0000

    add clang

--- cmake/CompilerFlags.cmake
+++ cmake/CompilerFlags.cmake
@@ -5,26 +5,26 @@ set(CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE} -DNDEBUG")
 set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -DNDEBUG")
 
 # GNU
-if(CMAKE_C_COMPILER_ID STREQUAL "GNU")
+if(CMAKE_C_COMPILER_ID MATCHES "GNU" OR CMAKE_C_COMPILER_ID MATCHES "(Apple)?[Cc]lang")
 	message("${BoldBlue}GNU C Compiler detected. Updating flags ...${ColourReset}")
 	set(CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG} -O0 -g --coverage -fdiagnostics-color=always")
     set(CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE} -O3 -fdiagnostics-color=always")
 endif()
 
-if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+if(CMAKE_CXX_COMPILER_ID MATCHES "GNU" OR CMAKE_CXX_COMPILER_ID MATCHES "(Apple)?[Cc]lang")
 	message("${BoldBlue}GNU C++ Compiler detected. Updating flags ...${ColourReset}")
 	set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -O0 -g --coverage -fdiagnostics-color=always")
     set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -O3 -fdiagnostics-color=always")
 endif()
 
 # Intel Compiler
-if(CMAKE_C_COMPILER_ID STREQUAL "Intel")
+if(CMAKE_C_COMPILER_ID MATCHES "Intel")
 	message("${BoldBlue}Intel C Compiler detected. Updating flags ...${ColourReset}")
 	set(CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG} -O0 -g -xHost -use-intel-optimized-headers -static-intel")
 	set(CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE} -O0 -g -xHost -ipo -use-intel-optimized-headers -static-intel")
 endif()
 
-if(CMAKE_CXX_COMPILER_ID STREQUAL "Intel")
+if(CMAKE_CXX_COMPILER_ID MATCHES "Intel")
 	message("${BoldBlue}Intel C++ Compiler detected. Updating flags ...${ColourReset}")
 	set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -O3 -xHost -use-intel-optimized-headers -static-intel")
 	set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -O3 -xHost -ipo -use-intel-optimized-headers -static-intel")
