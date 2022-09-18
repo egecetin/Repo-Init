89b5a85b5f9e54390b9f27ea0d91ef830d3df662
Ege Çetin <64282645+egecetin@users.noreply.github.com>
2022-09-18 15:08:41 +0300

    Compiler options (#9)
    * update cmake options
    
    * disable werr
    
    * disable weff for less thirdparty warn at default
    
    * reorder
    
    * add clang

--- cmake/GraphViz.cmake
+++ cmake/GraphViz.cmake
@@ -1,23 +1,23 @@
 find_program(DOT_EXE "dot")
 
 if(DOT_EXE)
-    message(STATUS "dot found: ${DOT_EXE}")
+	message(STATUS "dot found: ${DOT_EXE}")
 else()
-    message(STATUS "${BoldYellow}dot not found!${ColourReset}")
+	message(STATUS "${BoldYellow}dot not found!${ColourReset}")
 endif()
 
 set(DOT_OUTPUT_TYPE "svg" CACHE STRING "Build a dependency graph. Options are dot output types: ps, png, pdf...")
 
 if(DOT_EXE)
-    add_custom_target(dependency-graph
-        COMMAND ${CMAKE_COMMAND} ${PROJECT_SOURCE_DIR} --graphviz=${PROJECT_BINARY_DIR}/graphviz/${PROJECT_NAME}.dot
-        COMMAND ${DOT_EXE} -T${DOT_OUTPUT_TYPE} ${PROJECT_BINARY_DIR}/graphviz/${PROJECT_NAME}.dot -o ${PROJECT_BINARY_DIR}/${PROJECT_NAME}-tree.${DOT_OUTPUT_TYPE}
-    )
+	add_custom_target(dependency-graph
+		COMMAND ${CMAKE_COMMAND} ${PROJECT_SOURCE_DIR} --graphviz=${PROJECT_BINARY_DIR}/graphviz/${PROJECT_NAME}.dot
+		COMMAND ${DOT_EXE} -T${DOT_OUTPUT_TYPE} ${PROJECT_BINARY_DIR}/graphviz/${PROJECT_NAME}.dot -o ${PROJECT_BINARY_DIR}/${PROJECT_NAME}-tree.${DOT_OUTPUT_TYPE}
+	)
 
-    add_custom_command(
-        TARGET dependency-graph POST_BUILD
-        COMMAND ;
-        COMMENT
-        "Dependency graph generated and located at ${CMAKE_BINARY_DIR}/${PROJECT_NAME}-tree.${DOT_OUTPUT_TYPE}"
-    )
+	add_custom_command(
+		TARGET dependency-graph POST_BUILD
+		COMMAND ;
+		COMMENT
+		"Dependency graph generated and located at ${CMAKE_BINARY_DIR}/${PROJECT_NAME}-tree.${DOT_OUTPUT_TYPE}"
+	)
 endif()
