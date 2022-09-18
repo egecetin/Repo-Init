03e8ffe9c22a4708e1c3a1909411362e08cf66e8
Ege Çetin <egecetin@hotmail.com.tr>
2022-09-18 11:54:22 +0000

    reorder

--- cmake/CompilerFlags.cmake
+++ cmake/CompilerFlags.cmake
@@ -4,19 +4,6 @@ link_time_optimization(OPTIONAL)
 set(CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE} -DNDEBUG")
 set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -DNDEBUG")
 
-# Intel Compiler
-if(CMAKE_C_COMPILER_ID STREQUAL "Intel")
-	message("${BoldBlue}Intel C Compiler detected. Updating flags ...${ColourReset}")
-	set(CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG} -O0 -g -xHost -use-intel-optimized-headers -static-intel")
-	set(CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE} -O0 -g -xHost -ipo -use-intel-optimized-headers -static-intel")
-endif()
-
-if(CMAKE_CXX_COMPILER_ID STREQUAL "Intel")
-	message("${BoldBlue}Intel C++ Compiler detected. Updating flags ...${ColourReset}")
-	set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -O3 -xHost -use-intel-optimized-headers -static-intel")
-	set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -O3 -xHost -ipo -use-intel-optimized-headers -static-intel")
-endif()
-
 # GNU
 if(CMAKE_C_COMPILER_ID STREQUAL "GNU")
 	message("${BoldBlue}GNU C Compiler detected. Updating flags ...${ColourReset}")
@@ -30,4 +17,15 @@ if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
     set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -O3 -fdiagnostics-color=always")
 endif()
 
-# MSVC
+# Intel Compiler
+if(CMAKE_C_COMPILER_ID STREQUAL "Intel")
+	message("${BoldBlue}Intel C Compiler detected. Updating flags ...${ColourReset}")
+	set(CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG} -O0 -g -xHost -use-intel-optimized-headers -static-intel")
+	set(CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE} -O0 -g -xHost -ipo -use-intel-optimized-headers -static-intel")
+endif()
+
+if(CMAKE_CXX_COMPILER_ID STREQUAL "Intel")
+	message("${BoldBlue}Intel C++ Compiler detected. Updating flags ...${ColourReset}")
+	set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -O3 -xHost -use-intel-optimized-headers -static-intel")
+	set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -O3 -xHost -ipo -use-intel-optimized-headers -static-intel")
+endif()
