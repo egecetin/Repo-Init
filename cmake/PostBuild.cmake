c05ac5623f69f713e450d26df0d75a384950065d
Ege Çetin <egecetin@hotmail.com.tr>
2022-09-27 15:02:34 +0000

    md5/sha1/sha256 checksum for binaries

--- cmake/PostBuild.cmake
+++ cmake/PostBuild.cmake
@@ -34,5 +34,28 @@ if (${CMAKE_BUILD_TYPE} STREQUAL "Release")
 							"${PROJECT_SOURCE_DIR}/dist/makeself/"
 							)
 
-	message(STATUS "${PROJECT_NAME}.${PROJECT_VERSION_MAJOR}.${PROJECT_VERSION_MINOR}.${PROJECT_VERSION_PATCH}.run generated")
+	# Calculate MD5 checksums
+	message(STATUS "${BoldBlue}Calculating MD5 checksums for binaries${ColourReset}")
+	execute_process(COMMAND "find" "-type" "f" "-exec" "md5sum" "{}" "+"
+					WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}/dist/temp"
+					OUTPUT_VARIABLE md5Str)
+
+	# Calculate SHA1 checksums
+	message(STATUS "${BoldBlue}Calculating SHA1 checksums for binaries${ColourReset}")
+	execute_process(COMMAND "find" "-type" "f" "-exec" "sha1sum" "{}" "+"
+					WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}/dist/temp"
+					OUTPUT_VARIABLE sha1Str)
+
+	# Calculate SHA256 checksums
+	message(STATUS "${BoldBlue}Calculating SHA256 checksums for binaries${ColourReset}")
+	execute_process(COMMAND "find" "-type" "f" "-exec" "sha256sum" "{}" "+"
+					WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}/dist/temp"
+					OUTPUT_VARIABLE sha256Str)
+
+	# Write to file
+	file(WRITE "${PROJECT_SOURCE_DIR}/dist/temp/checksum.md5" ${md5Str})
+	file(WRITE "${PROJECT_SOURCE_DIR}/dist/temp/checksum.sha1" ${sha1Str})
+	file(WRITE "${PROJECT_SOURCE_DIR}/dist/temp/checksum.sha256" ${sha256Str})
+
+	message(STATUS "${BoldBlue}${PROJECT_NAME}.${PROJECT_VERSION_MAJOR}.${PROJECT_VERSION_MINOR}.${PROJECT_VERSION_PATCH}.run generated${ColourReset}")
 endif()
