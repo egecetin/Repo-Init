15f034f1aa84b5f2f89cbeca2c5f7c7cbb479c9b
Ege Çetin <64282645+egecetin@users.noreply.github.com>
2022-10-01 00:00:24 +0300

    CMake-Lint (#13)

--- cmake/GraphViz.cmake
+++ cmake/GraphViz.cmake
@@ -1,38 +1,37 @@
 #
 # Copyright (C) 2019 by George Cave - gcave@stablecoder.ca
 #
-# Licensed under the Apache License, Version 2.0 (the "License"); you may not
-# use this file except in compliance with the License. You may obtain a copy of
-# the License at
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+# the License. You may obtain a copy of the License at
 #
 # http://www.apache.org/licenses/LICENSE-2.0
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-# License for the specific language governing permissions and limitations under
-# the License.
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
 
 find_program(DOT_EXE "dot")
 
 if(DOT_EXE)
-	message(STATUS "dot found: ${DOT_EXE}")
+  message(STATUS "dot found: ${DOT_EXE}")
 else()
-	message(STATUS "${BoldYellow}dot not found!${ColourReset}")
+  message(STATUS "${BoldYellow}dot not found!${ColourReset}")
 endif()
 
-set(DOT_OUTPUT_TYPE "svg" CACHE STRING "Build a dependency graph. Options are dot output types: ps, png, pdf...")
+set(DOT_OUTPUT_TYPE
+    "svg"
+    CACHE STRING "Build a dependency graph. Options are dot output types: ps, png, pdf...")
 
 if(DOT_EXE)
-	add_custom_target(dependency-graph
-		COMMAND ${CMAKE_COMMAND} ${PROJECT_SOURCE_DIR} --graphviz=${PROJECT_BINARY_DIR}/graphviz/${PROJECT_NAME}.dot
-		COMMAND ${DOT_EXE} -T${DOT_OUTPUT_TYPE} ${PROJECT_BINARY_DIR}/graphviz/${PROJECT_NAME}.dot -o ${PROJECT_BINARY_DIR}/${PROJECT_NAME}-tree.${DOT_OUTPUT_TYPE}
-	)
+  add_custom_target(
+    dependency-graph
+    COMMAND ${CMAKE_COMMAND} ${PROJECT_SOURCE_DIR} --graphviz=${PROJECT_BINARY_DIR}/graphviz/${PROJECT_NAME}.dot
+    COMMAND ${DOT_EXE} -T${DOT_OUTPUT_TYPE} ${PROJECT_BINARY_DIR}/graphviz/${PROJECT_NAME}.dot -o
+            ${PROJECT_BINARY_DIR}/${PROJECT_NAME}-tree.${DOT_OUTPUT_TYPE})
 
-	add_custom_command(
-		TARGET dependency-graph POST_BUILD
-		COMMAND ;
-		COMMENT
-		"Dependency graph generated and located at ${CMAKE_BINARY_DIR}/${PROJECT_NAME}-tree.${DOT_OUTPUT_TYPE}"
-	)
+  add_custom_command(
+    TARGET dependency-graph
+    POST_BUILD
+    COMMAND ;
+    COMMENT "Dependency graph generated and located at ${CMAKE_BINARY_DIR}/${PROJECT_NAME}-tree.${DOT_OUTPUT_TYPE}")
 endif()
--- cmake/PostBuild.cmake
+++ cmake/PostBuild.cmake
@@ -2,60 +2,55 @@
 include(Colorize)
 include(CMakeCacheForScript)
 
-if (${CMAKE_BUILD_TYPE} STREQUAL "Release")
+if(${CMAKE_BUILD_TYPE} STREQUAL "Release")
 
-	message(STATUS "${BoldBlue}Updating dist folder and preparing packages${ColourReset}")
-	# Clear old files
-	execute_process(COMMAND "find" "${PROJECT_SOURCE_DIR}/dist/temp/" "-maxdepth" "1" "-type" "f" "-delete")
+  message(STATUS "${BoldBlue}Updating dist folder and preparing packages${ColourReset}")
+  # Clear old files
+  execute_process(COMMAND "find" "${PROJECT_SOURCE_DIR}/dist/temp/" "-maxdepth" "1" "-type" "f" "-delete")
 
-	# Copy shared libraries
-	execute_process(COMMAND "sh"
-							"${PROJECT_SOURCE_DIR}/scripts/ldd-copy-dependencies.sh"
-							"-b"
-							"${CMAKE_RUNTIME_OUTPUT_DIRECTORY_RELEASE}/${PROJECT_NAME}"
-							"-t"
-							"${PROJECT_SOURCE_DIR}/dist/temp"
-							)
+  # Copy shared libraries
+  execute_process(
+    COMMAND "sh" "${PROJECT_SOURCE_DIR}/scripts/ldd-copy-dependencies.sh" "-b"
+            "${CMAKE_RUNTIME_OUTPUT_DIRECTORY_RELEASE}/${PROJECT_NAME}" "-t" "${PROJECT_SOURCE_DIR}/dist/temp")
 
-	# Create makeself package
-	execute_process(COMMAND "sh"
-							"${PROJECT_SOURCE_DIR}/scripts/makeself/makeself.sh"
-							"--notemp"
-							"--sha256"
-							"${PROJECT_SOURCE_DIR}/dist/temp"
-							"${PROJECT_NAME}.${PROJECT_VERSION_MAJOR}.${PROJECT_VERSION_MINOR}.${PROJECT_VERSION_PATCH}.run"
-							"${PROJECT_NAME}"
-							)
+  # Create makeself package
+  execute_process(
+    COMMAND
+      "sh" "${PROJECT_SOURCE_DIR}/scripts/makeself/makeself.sh" "--notemp" "--sha256" "${PROJECT_SOURCE_DIR}/dist/temp"
+      "${PROJECT_NAME}.${PROJECT_VERSION_MAJOR}.${PROJECT_VERSION_MINOR}.${PROJECT_VERSION_PATCH}.run"
+      "${PROJECT_NAME}")
 
-	# Move to another directory
-	execute_process(COMMAND "mv"
-							"-f"
-							"${PROJECT_NAME}.${PROJECT_VERSION_MAJOR}.${PROJECT_VERSION_MINOR}.${PROJECT_VERSION_PATCH}.run"
-							"${PROJECT_SOURCE_DIR}/dist/makeself/"
-							)
+  # Move to another directory
+  execute_process(
+    COMMAND "mv" "-f" "${PROJECT_NAME}.${PROJECT_VERSION_MAJOR}.${PROJECT_VERSION_MINOR}.${PROJECT_VERSION_PATCH}.run"
+            "${PROJECT_SOURCE_DIR}/dist/makeself/")
 
-	# Calculate MD5 checksums
-	message(STATUS "${BoldBlue}Calculating MD5 checksums for binaries${ColourReset}")
-	execute_process(COMMAND "find" "-type" "f" "-exec" "md5sum" "{}" "+"
-					WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}/dist/temp"
-					OUTPUT_VARIABLE md5Str)
+  # Calculate MD5 checksums
+  message(STATUS "${BoldBlue}Calculating MD5 checksums for binaries${ColourReset}")
+  execute_process(
+    COMMAND "find" "-type" "f" "-exec" "md5sum" "{}" "+"
+    WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}/dist/temp"
+    OUTPUT_VARIABLE md5Str)
 
-	# Calculate SHA1 checksums
-	message(STATUS "${BoldBlue}Calculating SHA1 checksums for binaries${ColourReset}")
-	execute_process(COMMAND "find" "-type" "f" "-exec" "sha1sum" "{}" "+"
-					WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}/dist/temp"
-					OUTPUT_VARIABLE sha1Str)
+  # Calculate SHA1 checksums
+  message(STATUS "${BoldBlue}Calculating SHA1 checksums for binaries${ColourReset}")
+  execute_process(
+    COMMAND "find" "-type" "f" "-exec" "sha1sum" "{}" "+"
+    WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}/dist/temp"
+    OUTPUT_VARIABLE sha1Str)
 
-	# Calculate SHA256 checksums
-	message(STATUS "${BoldBlue}Calculating SHA256 checksums for binaries${ColourReset}")
-	execute_process(COMMAND "find" "-type" "f" "-exec" "sha256sum" "{}" "+"
-					WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}/dist/temp"
-					OUTPUT_VARIABLE sha256Str)
+  # Calculate SHA256 checksums
+  message(STATUS "${BoldBlue}Calculating SHA256 checksums for binaries${ColourReset}")
+  execute_process(
+    COMMAND "find" "-type" "f" "-exec" "sha256sum" "{}" "+"
+    WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}/dist/temp"
+    OUTPUT_VARIABLE sha256Str)
 
-	# Write to file
-	file(WRITE "${PROJECT_SOURCE_DIR}/dist/temp/checksum.md5" ${md5Str})
-	file(WRITE "${PROJECT_SOURCE_DIR}/dist/temp/checksum.sha1" ${sha1Str})
-	file(WRITE "${PROJECT_SOURCE_DIR}/dist/temp/checksum.sha256" ${sha256Str})
+  # Write to file
+  file(WRITE "${PROJECT_SOURCE_DIR}/dist/temp/checksum.md5" ${md5Str})
+  file(WRITE "${PROJECT_SOURCE_DIR}/dist/temp/checksum.sha1" ${sha1Str})
+  file(WRITE "${PROJECT_SOURCE_DIR}/dist/temp/checksum.sha256" ${sha256Str})
 
-	message(STATUS "${BoldBlue}${PROJECT_NAME}.${PROJECT_VERSION_MAJOR}.${PROJECT_VERSION_MINOR}.${PROJECT_VERSION_PATCH}.run generated${ColourReset}")
+  message(STATUS "${BoldBlue}${PROJECT_NAME}.${PROJECT_VERSION_MAJOR}.${PROJECT_VERSION_MINOR}"
+                 ".${PROJECT_VERSION_PATCH}.run generated${ColourReset}")
 endif()
