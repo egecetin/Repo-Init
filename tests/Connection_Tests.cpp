e8c7239bb53ac865dcab5182f6942579f2dc24ff
Ege Çetin <egecetin@hotmail.com.tr>
2022-10-01 13:47:10 +0000

    fix pre-commit

--- tests/Connection_Tests.cpp
+++ tests/Connection_Tests.cpp
@@ -11,61 +11,61 @@
 
 TEST(Connection_Tests, HttpTests)
 {
-    // python -m restapi_echo_server --host 0.0.0.0 --port 8080
-    // Launch echo server
+	// python -m restapi_echo_server --host 0.0.0.0 --port 8080
+	// Launch echo server
 	std::future<int> pyResult;
-    pyResult = std::async(std::launch::async, []() {
+	pyResult = std::async(std::launch::async, []() {
 		return system(("python3 " + std::string(TEST_REST_ECHO_SERVER_PY_PATH) + " >/dev/null").c_str());
 	});
 	std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 
-    HTTP handler("http://127.0.0.1:8080");
+	HTTP handler("http://127.0.0.1:8080");
 
-    std::string recvData;
-    handler.sendPOSTRequest("", "Test POST Message", recvData);
-    ASSERT_EQ("Test POST Message", recvData);
-    handler.sendGETRequest("", recvData);
-    ASSERT_EQ("", recvData);
+	std::string recvData;
+	handler.sendPOSTRequest("", "Test POST Message", recvData);
+	ASSERT_EQ("Test POST Message", recvData);
+	handler.sendGETRequest("", recvData);
+	ASSERT_EQ("", recvData);
 }
 
 TEST(Connection_Tests, ZeroMQTests)
 {
 	// Internally used by ZeroMQ sessions
-    ZMQ_SEND_TIMEOUT = 1000;
-    ZMQ_RECV_TIMEOUT = 1000;
+	ZMQ_SEND_TIMEOUT = 1000;
+	ZMQ_RECV_TIMEOUT = 1000;
 
-    // Launch echo server
+	// Launch echo server
 	std::future<int> pyResult;
-    pyResult = std::async(std::launch::async, []() {
+	pyResult = std::async(std::launch::async, []() {
 		return system(("python3 " + std::string(TEST_ZEROMQ_ECHO_SERVER_PY_PATH) + " >/dev/null").c_str());
 	});
 	std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 
-    std::shared_ptr<zmq::context_t> ctx = std::make_shared<zmq::context_t>(1);
-    ZeroMQ handler(ctx, zmq::socket_type::req, TEST_ZEROMQ_ECHO_SERVER_ADDR, false);
+	std::shared_ptr<zmq::context_t> ctx = std::make_shared<zmq::context_t>(1);
+	ZeroMQ handler(ctx, zmq::socket_type::req, TEST_ZEROMQ_ECHO_SERVER_ADDR, false);
 
-    int testData1 = 15;
-    double testData2 = 3.14;
-    std::string testData3 = "Test Message";
+	int testData1 = 15;
+	double testData2 = 3.14;
+	std::string testData3 = "Test Message";
 
-    std::vector<zmq::const_buffer> vSendMsg;
-    vSendMsg.push_back(zmq::const_buffer(&testData1, sizeof(testData1)));
-    vSendMsg.push_back(zmq::const_buffer(&testData2, sizeof(testData2)));
-    vSendMsg.push_back(zmq::const_buffer(testData3.c_str(), testData3.size()));
+	std::vector<zmq::const_buffer> vSendMsg;
+	vSendMsg.push_back(zmq::const_buffer(&testData1, sizeof(testData1)));
+	vSendMsg.push_back(zmq::const_buffer(&testData2, sizeof(testData2)));
+	vSendMsg.push_back(zmq::const_buffer(testData3.c_str(), testData3.size()));
 
-    ASSERT_EQ(handler.sendMessages(vSendMsg), vSendMsg.size());
+	ASSERT_EQ(handler.sendMessages(vSendMsg), vSendMsg.size());
 
-    auto vRecvMsgs = handler.recvMessages();
-    ASSERT_EQ(vSendMsg.size(), vRecvMsgs.size());
-    
-    for (size_t idx = 0; idx < vSendMsg.size(); ++idx)
-    {
-        ASSERT_EQ(vSendMsg[idx].size(), vRecvMsgs[idx].size());
-        ASSERT_FALSE(memcmp(vSendMsg[idx].data(), vRecvMsgs[idx].data(), vSendMsg[idx].size()));
-    }
+	auto vRecvMsgs = handler.recvMessages();
+	ASSERT_EQ(vSendMsg.size(), vRecvMsgs.size());
+
+	for (size_t idx = 0; idx < vSendMsg.size(); ++idx)
+	{
+		ASSERT_EQ(vSendMsg[idx].size(), vRecvMsgs[idx].size());
+		ASSERT_FALSE(memcmp(vSendMsg[idx].data(), vRecvMsgs[idx].data(), vSendMsg[idx].size()));
+	}
 
 	pyResult.wait();
 	ASSERT_EQ(0, pyResult.get());
 
-    ZeroMQ handler2(ctx, zmq::socket_type::rep, TEST_ZEROMQ_ECHO_SERVER_ADDR2, true);
+	ZeroMQ handler2(ctx, zmq::socket_type::rep, TEST_ZEROMQ_ECHO_SERVER_ADDR2, true);
 }
--- tests/data/restEchoServer.py
+++ tests/data/restEchoServer.py
@@ -5,30 +5,30 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from optparse import OptionParser
 
+
 class RequestHandler(BaseHTTPRequestHandler):
-    
     def do_GET(self):
-        
+
         request_path = self.path
 
         print("\n----- Request Start ----->\n")
         print("Request path:", request_path)
         print("Request headers:", self.headers)
         print("<----- Request End -----\n")
-        
+
         self.send_response(200)
         self.send_header("Set-Cookie", "foo=bar")
         self.end_headers()
-        
+
     def do_POST(self):
-        
+
         request_path = self.path
-        
+
         print("\n----- Request Start ----->\n")
         print("Request path:", request_path)
-        
+
         request_headers = self.headers
-        content_length = request_headers.get('Content-Length')
+        content_length = request_headers.get("Content-Length")
         length = int(content_length) if content_length else 0
         payload = self.rfile.read(length)
 
@@ -36,27 +36,30 @@ class RequestHandler(BaseHTTPRequestHandler):
         print("Request headers:", request_headers)
         print("Request payload:", payload)
         print("<----- Request End -----\n")
-        
+
         self.send_response(200)
         self.end_headers()
         self.wfile.write(payload)
-    
+
     do_PUT = do_POST
     do_DELETE = do_GET
-        
+
+
 def main():
     port = 8080
-    print('Listening on 0.0.0.0:%s' % port)
-    server = HTTPServer(('', port), RequestHandler)
+    print("Listening on 0.0.0.0:%s" % port)
+    server = HTTPServer(("", port), RequestHandler)
     server.handle_request()
     server.handle_request()
 
-        
+
 if __name__ == "__main__":
     parser = OptionParser()
-    parser.usage = ("Creates an http-server that will echo out any GET or POST parameters\n"
-                    "Run:\n\n"
-                    "   reflect")
+    parser.usage = (
+        "Creates an http-server that will echo out any GET or POST parameters\n"
+        "Run:\n\n"
+        "   reflect"
+    )
     (options, args) = parser.parse_args()
-    
+
     main()
