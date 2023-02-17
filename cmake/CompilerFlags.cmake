2a65f4dab14213b51a853b71cf4f871331b8b097
Ege Çetin <egecetin@hotmail.com.tr>
2023-02-17 16:00:08 +0000

    Update LTO

--- cmake/CompilerFlags.cmake
+++ cmake/CompilerFlags.cmake
@@ -1,6 +1,4 @@
-include(LinkTimeOptimization)
 
-link_time_optimization()
 set(CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE} -DNDEBUG")
 set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -DNDEBUG")
 
@@ -21,11 +19,11 @@ endif()
 if(CMAKE_C_COMPILER_ID MATCHES "Intel")
   message("${BoldBlue}Intel C Compiler detected. Updating flags ...${ColourReset}")
   set(CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG} -O0 -g -xHost -ftest-coverage -fprofile-arcs")
-  set(CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE} -O3 -xHost")
+  set(CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE} -O3 -xHost -ipo")
 endif()
 
 if(CMAKE_CXX_COMPILER_ID MATCHES "Intel")
   message("${BoldBlue}Intel C++ Compiler detected. Updating flags ...${ColourReset}")
   set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -O0 -g -xHost -ftest-coverage -fprofile-arcs")
-  set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -O3 -xHost")
+  set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -O3 -xHost -ipo")
 endif()
