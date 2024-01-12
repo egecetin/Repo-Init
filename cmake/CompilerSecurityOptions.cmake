d17bf9635c8f714af8c0733e345504a99ab4f282
Ege Çetin <egecetin@hotmail.com.tr>
2024-01-12 14:59:07 +0300

    update

--- cmake/CompilerSecurityOptions.cmake
+++ cmake/CompilerSecurityOptions.cmake
@@ -10,51 +10,46 @@
 # "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 # specific language governing permissions and limitations under the License.
 
-option(ENABLE_RECOMMENDED_SECURITY_FLAGS "Compile all targets with recommended security hardening flags. Check https://best.openssf.org/Compiler-Hardening-Guides/Compiler-Options-Hardening-Guide-for-C-and-C++" OFF)
+option(ENABLE_RECOMMENDED_SECURITY_FLAGS "Compile all targets with recommended security hardening flags. Check \
+https://best.openssf.org/Compiler-Hardening-Guides/Compiler-Options-Hardening-Guide-for-C-and-C++ \
+for information" OFF)
 
-set(COMPILER_SECURE_FLAGS -Wconversion -Wtrampolines -Wimplicit-fallthrough
--U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=3
--D_GLIBCXX_ASSERTIONS
--fstrict-flex-arrays=3
--fstack-clash-protection -fstack-protector-strong
--Wl,-z,nodlopen -Wl,-z,noexecstack
--Wl,-z,relro -Wl,-z,now)
+set(COMPILER_SECURE_FLAGS_ENABLED "-Wconversion;-Wtrampolines;-Wimplicit-fallthrough;-U_FORTIFY_SOURCE;"
+  "-D_FORTIFY_SOURCE=3;-D_GLIBCXX_ASSERTIONS;-fstrict-flex-arrays=3;-fstack-clash-protection;"
+  "-fstack-protector-strong;-Wl,-z,nodlopen;-Wl,-z,noexecstack;-Wl,-z,relro;-Wl,-z,now")
+set(COMPILER_SECURE_FLAGS_DISABLED "")
 
-function(enable_security_flags_for_target)
-    set(options NONE)
-    set(oneValueArgs TARGET NAME)
-    cmake_parse_arguments(SECURE "${options}" "${oneValueArgs}" ${ARGN})
+if(CMAKE_CXX_COMPILER_ID MATCHES "GNU")
+  if(CMAKE_CXX_COMPILER_VERSION VERSION_LESS "13.0")
+    list(APPEND COMPILER_SECURE_FLAGS_DISABLED "-fstrict-flex-arrays=3")
+    if(CMAKE_CXX_COMPILER_VERSION VERSION_LESS "12.0")
+      list(APPEND COMPILER_SECURE_FLAGS_DISABLED "-U_FORTIFY_SOURCE;-D_FORTIFY_SOURCE=3")
+      if(CMAKE_CXX_COMPILER_VERSION VERSION_LESS "8.0")
+        message(ERROR "GNU compiler version less than 8.0. Security flags are not supported")
+      endif() # Version 8.0
+    endif() # Version 12.0
+  endif() # Version 13.0
+elseif(CMAKE_CXX_COMPILER_ID MATCHES "(Apple)?[Cc]lang")
+  if(CMAKE_CXX_COMPILER_VERSION VERSION_LESS "16.0")
+    list(APPEND COMPILER_SECURE_FLAGS_DISABLED "-fstrict-flex-arrays=3")
+    if(CMAKE_CXX_COMPILER_VERSION VERSION_LESS "12.0")
+      list(APPEND COMPILER_SECURE_FLAGS_DISABLED "-U_FORTIFY_SOURCE;-D_FORTIFY_SOURCE=3")
+      if(CMAKE_CXX_COMPILER_VERSION VERSION_LESS "11.0")
+        message(ERROR "Clang compiler version less than 11.0. Security flags are not supported")
+      endif() # Version 11.0
+    endif() # Version 12.0
+  endif() # Version 16.0
+else()
+  message(ERROR "Compiler ${CMAKE_CXX_COMPILER_ID} not supported for hardening flags")
+endif()
 
-    if(CMAKE_CXX_COMPILER_ID MATCHES "GNU")
-        if(CMAKE_CXX_COMPILER_VERSION VERSION_LESS "13.0")
-            list(APPEND DISABLED_OPTIONS "-fstrict-flex-arrays=3")
-            if(CMAKE_CXX_COMPILER_VERSION VERSION_LESS "12.0")
-                list(APPEND DISABLED_OPTIONS "-U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=3")
-                if(CMAKE_CXX_COMPILER_VERSION VERSION_LESS "9.0")
-                    message(ERROR "GNU compiler version less than 9.0. Security flags are not supported for older versions with CompilerSecurityOptions.cmake. Please enable supported flags manually")
-                else()
-                    # All flags except strict-flex-arrays and fortify source
-                    target_compile_options(SECURE_TARGET PRIVATE
-                    -Wconversion -Wtrampolines -Wimplicit-fallthrough
-                    -fstack-clash-protection -fstack-protector-strong
-                    -Wl,-z,nodlopen -Wl,-z,noexecstack -Wl,-z,relro -Wl,-z,now)
-                    message(WARNING "GNU compiler version less than 12 following flags are not supported: ${DISABLED_OPTIONS}")
-                endif()
-            else()
-                # All flags except strict-flex-arrays
-                target_compile_options(SECURE_TARGET PRIVATE
-                    -U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=3
-                    -Wconversion -Wtrampolines -Wimplicit-fallthrough
-                    -fstack-clash-protection -fstack-protector-strong
-                    -Wl,-z,nodlopen -Wl,-z,noexecstack -Wl,-z,relro -Wl,-z,now)
-                message(WARNING "GNU compiler version less than 13 following flags are not supported: ${DISABLED_OPTIONS}")
-            endif()
-        else()
-            # Enable all flags
-        endif()
-    elseif(CMAKE_CXX_COMPILER_ID MATCHES "(Apple)?[Cc]lang")
-        if(CMAKE_CXX_COMPILER_VERSION VERSION_LESS "your.required.gcc.version")
-        else()
-        endif()
-    endif()
-endfunction(enable_security_flags_for_target)
+list(REMOVE_ITEM COMPILER_SECURE_FLAGS_ENABLED ${COMPILER_SECURE_FLAGS_DISABLED})
+
+# Enable flags
+message(STATUS "Supported hardening flags ${COMPILER_SECURE_FLAGS_ENABLED}")
+message(WARNING "Not supported hardening flags ${COMPILER_SECURE_FLAGS_DISABLED}")
+
+macro(enable_security_flags_for_target name)
+  message(STATUS "Enable hardening flags for target ${name}")
+  target_compile_options(${name} PRIVATE ${COMPILER_SECURE_FLAGS_ENABLED})
+endmacro()
