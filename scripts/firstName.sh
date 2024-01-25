d59a9f3b8ec26a09444fe2aa65414628832f00ea
Ege Çetin <egecetin@hotmail.com.tr>
2024-01-25 16:24:26 +0300

    Script should fail for naming errors

--- scripts/firstName.sh
+++ scripts/firstName.sh
@@ -8,6 +8,7 @@ do
 done
 
 [ -z "${ARGUMENT_NAME}" ] && echo "$0: Missing argument: [-n name]" >&2
+[ -z "${ARGUMENT_NAME}" ] && exit 1
 
 sed -i "s/XXX/${ARGUMENT_NAME}/" CMakeLists.txt
 sed -i "s/XXX/${ARGUMENT_NAME}/" CMakeLists.txt
