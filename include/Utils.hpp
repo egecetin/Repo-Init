fc9dc7809b765bb3db067b105abecaafb09d0cdf
Ege Çetin <egecetin@hotmail.com.tr>
2024-03-21 16:20:29 +0300

    wip utils

--- include/Utils.hpp
+++ include/Utils.hpp
@@ -28,58 +28,6 @@ extern std::vector<std::pair<std::string, std::shared_ptr<std::atomic_flag>>> vC
 /* ################################################################################### */
 
 /**
- * @class InputParser
- * @brief Parses command line inputs
- */
-class InputParser {
-  private:
-	std::vector<std::string> tokens;
-
-  public:
-	/**
-	 * @brief Constructs a new InputParser object
-	 * @param[in] argc Number of input arguments
-	 * @param[in] argv Input arguments
-	 */
-	InputParser(const int &argc, char **argv)
-	{
-		for (int i = 1; i < argc; ++i)
-		{
-			this->tokens.emplace_back(argv[i]);
-		}
-		this->tokens.emplace_back("");
-	}
-
-	/**
-	 * @brief Gets single command line input
-	 * @param[in] option Option to check
-	 * @return const std::string& Found command line input. Empty string if not found
-	 */
-	const std::string &getCmdOption(const std::string &option) const
-	{
-		std::vector<std::string>::const_iterator itr;
-		itr = std::find(this->tokens.begin(), this->tokens.end(), option);
-		if (itr != this->tokens.end() && ++itr != this->tokens.end())
-		{
-			return *itr;
-		}
-		static const std::string empty_string;
-		return empty_string;
-	}
-
-	/**
-	 * @brief Checks whether provided command line option exists.
-	 * @param[in] option Option to check
-	 * @return true If the provided option is found
-	 * @return false If the provided option is not found
-	 */
-	bool cmdOptionExists(const std::string &option) const
-	{
-		return std::find(this->tokens.begin(), this->tokens.end(), option) != this->tokens.end();
-	}
-};
-
-/**
  * @struct spinlock
  * @brief Spinlock implementation from https://rigtorp.se/spinlock/
  */
---
+++ include/utils/ConfigParser.hpp
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <string>
+#include <unordered_map>
+
+class ConfigParser 
+{
+    private:
+        std::string configPath;
+        std::unordered_map<std::string, std::string> configMap;
+
+    public:
+        /**
+         * @brief Construct a new Config Parser object from config file. Currently only json files are supported
+         * @param[in] configPath Path to the configuration file
+        */
+        ConfigParser(const std::string &configPath);
+
+        /**
+         * @brief Get the value of a key from the configuration file. If the configuration file modified from outside
+         * the program, the changes will not be reflected. You should call load() to refresh the configuration see the changes.
+         * @param[in] key Key to search for
+         * @return std::string Value of the key
+        */
+        std::string get(const std::string &key);
+
+        /**
+         * @brief Set the value of a key in the configuration file. If the key does not exist, it will be created
+         * If you don't save to the file, the changes will be lost.
+         * @param[in] key Key to set
+         * @param[in] value Value to set
+        */
+        void set(const std::string &key, const std::string &value);
+
+        /**
+         * @brief Save the configuration to the file
+        */
+        void save();
+
+        /**
+         * @brief Load the configuration from the file
+        */
+        void load();
+
+        /**
+         * @brief Print the configuration to the console
+        */
+        void print();
+}
