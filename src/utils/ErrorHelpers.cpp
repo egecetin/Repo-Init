92bc346ee1ab13394b21a904d15c30056aa55348
Ege Çetin <egecetin@hotmail.com.tr>
2024-03-24 18:17:50 +0300

    fix clang

--- src/utils/ErrorHelpers.cpp
+++ src/utils/ErrorHelpers.cpp
@@ -1,3 +1,4 @@
 #include "utils/ErrorHelpers.hpp"
 
+// NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
 std::vector<std::pair<std::string, std::shared_ptr<std::atomic_flag>>> vCheckFlag;
