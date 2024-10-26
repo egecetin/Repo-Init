aa874a1d82ec6bdb618b77c950d5c2356a4d9d9b
egecetin <egecetin@hotmail.com.tr>
2024-10-26 12:16:33 +0300

    minor format + add rocky runners

--- include/utils/ErrorHelpers.hpp
+++ include/utils/ErrorHelpers.hpp
@@ -19,8 +19,8 @@ extern std::vector<std::pair<std::string, std::shared_ptr<std::atomic_flag>>> vC
 inline std::string getErrnoString(int errVal)
 {
 	std::array<char, BUFSIZ> buffer{};
-	auto val = strerror_r(errVal, buffer.data(), BUFSIZ);
-#if (_POSIX_C_SOURCE >= 200112L || _XOPEN_SOURCE >= 600) && ! _GNU_SOURCE
+	auto *val = strerror_r(errVal, buffer.data(), BUFSIZ);
+#if (_POSIX_C_SOURCE >= 200112L || _XOPEN_SOURCE >= 600) && !_GNU_SOURCE
 	if (val != 0)
 	{
 		return "Cannot get error message";
