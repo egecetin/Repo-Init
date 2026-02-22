7ea651a58ced87d7de09ae26236786f00ea7f1e7
egecetin <egecetin@hotmail.com.tr>
2026-02-22 16:43:22 +0300

    enable C++20

--- scripts/runStaticAnalysis.sh
+++ scripts/runStaticAnalysis.sh
@@ -9,7 +9,7 @@ echo "Running clang-format"
 clang-format include/**/*.hpp src/*.cpp src/**/*.cpp --verbose --dry-run --Werror
 
 echo "Running cppcheck"
-cppcheck -Iinclude/ src --verbose --enable=all --error-exitcode=1 --std=c++17 --language=c++ --suppressions-list=cppcheckSuppressions.txt --inline-suppr
+cppcheck -Iinclude/ src --verbose --enable=all --error-exitcode=1 --std=c++20 --language=c++ --suppressions-list=cppcheckSuppressions.txt --inline-suppr
 
 echo "Running clang-tidy"
 run-clang-tidy -j$(nproc) -p=build -header-filter=$(pwd)/include/ src/*.cpp src/**/*.cpp
